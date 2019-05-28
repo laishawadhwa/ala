05b0697f27cd6ae3f7ae538ed59b3b9ea18acf37
Abhinav Hinger <abhinavhinger12@gmail.com>
2019-05-28 19:52:28 +0530

    opencv methods

--- detect.py
+++ detect.py
@@ -305,6 +305,36 @@ def detect_text(path):
     # [END vision_python_migration_text_detection]
 # [END vision_text_detection]
 
+# def detect_text(path):
+#     """Detects document features in an image."""
+#     from google.cloud import vision
+#     client = vision.ImageAnnotatorClient()
+
+#     with io.open(path, 'rb') as image_file:
+#         content = image_file.read()
+
+#     image = vision.types.Image(content=content)
+
+#     response = client.document_text_detection(image=image)
+
+#     for page in response.full_text_annotation.pages:
+#         for block in page.blocks:
+#             print('\nBlock confidence: {}\n'.format(block.confidence))
+
+#             for paragraph in block.paragraphs:
+#                 print('Paragraph confidence: {}'.format(
+#                     paragraph.confidence))
+
+#                 for word in paragraph.words:
+#                     word_text = ''.join([
+#                         symbol.text for symbol in word.symbols
+#                     ])
+#                     print('Word text: {} (confidence: {})'.format(
+#                         word_text, word.confidence))
+
+#                     for symbol in word.symbols:
+#                         print('\tSymbol: {} (confidence: {})'.format(
+#                             symbol.text, symbol.confidence))
 
 # [START vision_text_detection_gcs]
 def detect_text_uri(uri):
